1e933b7ca5077784fa44855d247aefa51b26c14e
Alex Rodriguez <alexrodriguezbenitez@gmail.com>
2025-11-11 17:16:49 -0600

    build: publish all modules to Maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,23 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
 }
+
+subprojects {
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.library") || plugins.hasPlugin("maven-publish")) {
+            extensions.findByType<PublishingExtension>()?.apply {
+                publications {
+                    create<MavenPublication>("release") {
+                        groupId = "com.github.alexrodben"
+                        artifactId = project.name // usa el nombre del módulo (api, core, etc.)
+                        version = rootProject.version.toString()
+
+                        afterEvaluate {
+                            from(components.findByName("release"))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
